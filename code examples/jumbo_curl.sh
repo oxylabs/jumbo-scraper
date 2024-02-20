fcf0552ee06f6569a04336329604d6c730f9bd33
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:45:57 +0200

    Adding code examples/jumbo_curl.sh

---
+++ code examples/jumbo_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://www.jumbo.com/producten/zuivel,-eieren,-boter/houdbare-melk-en-zuivel/"}'
